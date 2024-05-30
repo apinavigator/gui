ea0dea2d9231fad137e8e5d11f2de62da6f7887f
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-05-30 13:32:03 -0400

    Test the mock template generated by the Content CLI
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/components/subject.tsx
+++ src/components/subject.tsx
@@ -17,7 +17,7 @@ const Subject = ({ subject }: { subject: ISubject }) => {
 
   return (
     <article className='p-3 border-solid border-slate-100 sm:border'>
-      <h3 className='text-2xl font-semibold'>{subject.name}</h3>
+      <h3 className='text-xl font-semibold'>{subject.name}</h3>
       <div className='grid grid-cols-2 gap-4 mt-3'>
           {subject.topics.map((topic, index) => <Topic key={index} topic={topic} />)}
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import bash from 'react-syntax-highlighter/dist/esm/languages/hljs/bash';
+import cmake from 'react-syntax-highlighter/dist/esm/languages/hljs/cmake';
+import csharp from 'react-syntax-highlighter/dist/esm/languages/hljs/csharp';
+import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
+import dart from 'react-syntax-highlighter/dist/esm/languages/hljs/dart';
+import django from 'react-syntax-highlighter/dist/esm/languages/hljs/django';
+import dockerfile from 'react-syntax-highlighter/dist/esm/languages/hljs/dockerfile';
+import go from 'react-syntax-highlighter/dist/esm/languages/hljs/go';
+import haskell from 'react-syntax-highlighter/dist/esm/languages/hljs/haskell';
+import htmlbars from 'react-syntax-highlighter/dist/esm/languages/hljs/htmlbars';
+import http from 'react-syntax-highlighter/dist/esm/languages/hljs/http';
+import java from 'react-syntax-highlighter/dist/esm/languages/hljs/java';
+import javascript from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
+import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json';
+import latex from 'react-syntax-highlighter/dist/esm/languages/hljs/latex';
+import makefile from 'react-syntax-highlighter/dist/esm/languages/hljs/makefile';
+import markdown from 'react-syntax-highlighter/dist/esm/languages/hljs/markdown';
+import nginx from 'react-syntax-highlighter/dist/esm/languages/hljs/nginx';
+import perl from 'react-syntax-highlighter/dist/esm/languages/hljs/perl';
+import pgsql from 'react-syntax-highlighter/dist/esm/languages/hljs/pgsql';
+import php from 'react-syntax-highlighter/dist/esm/languages/hljs/php';
+import plaintext from 'react-syntax-highlighter/dist/esm/languages/hljs/plaintext';
+import powershell from 'react-syntax-highlighter/dist/esm/languages/hljs/powershell';
+import python from 'react-syntax-highlighter/dist/esm/languages/hljs/python';
+import ruby from 'react-syntax-highlighter/dist/esm/languages/hljs/ruby';
+import rust from 'react-syntax-highlighter/dist/esm/languages/hljs/rust';
+import scss from 'react-syntax-highlighter/dist/esm/languages/hljs/scss';
+import shell from 'react-syntax-highlighter/dist/esm/languages/hljs/shell';
+import sql from 'react-syntax-highlighter/dist/esm/languages/hljs/sql';
+import typescript from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
+import yaml from 'react-syntax-highlighter/dist/esm/languages/hljs/yaml';
+
 import { App } from './components/app.component.tsx';
 import './stylesheets/index.css';
+
+
+
+SyntaxHighlighter.registerLanguage('bash', bash);
+SyntaxHighlighter.registerLanguage('cmake', cmake);
+SyntaxHighlighter.registerLanguage('csharp', csharp);
+SyntaxHighlighter.registerLanguage('css', css);
+SyntaxHighlighter.registerLanguage('dart', dart);
+SyntaxHighlighter.registerLanguage('django', django);
+SyntaxHighlighter.registerLanguage('dockerfile', dockerfile);
+SyntaxHighlighter.registerLanguage('go', go);
+SyntaxHighlighter.registerLanguage('haskell', haskell);
+SyntaxHighlighter.registerLanguage('html', htmlbars);
+SyntaxHighlighter.registerLanguage('http', http);
+SyntaxHighlighter.registerLanguage('java', java);
+SyntaxHighlighter.registerLanguage('javascript', javascript);
+SyntaxHighlighter.registerLanguage('json', json);
+SyntaxHighlighter.registerLanguage('latex', latex);
+SyntaxHighlighter.registerLanguage('makefile', makefile);
+SyntaxHighlighter.registerLanguage('markdown', markdown);
+SyntaxHighlighter.registerLanguage('nginx', nginx);
+SyntaxHighlighter.registerLanguage('perl', perl);
+SyntaxHighlighter.registerLanguage('pgsql', pgsql);
+SyntaxHighlighter.registerLanguage('php', php);
+SyntaxHighlighter.registerLanguage('plaintext', plaintext);
+SyntaxHighlighter.registerLanguage('powershell', powershell);
+SyntaxHighlighter.registerLanguage('python', python);
+SyntaxHighlighter.registerLanguage('ruby', ruby);
+SyntaxHighlighter.registerLanguage('rust', rust);
+SyntaxHighlighter.registerLanguage('scss', scss);
+SyntaxHighlighter.registerLanguage('shell', shell);
+SyntaxHighlighter.registerLanguage('sql', sql);
+SyntaxHighlighter.registerLanguage('typescript', typescript);
+SyntaxHighlighter.registerLanguage('xml', xml);
+SyntaxHighlighter.registerLanguage('yaml', yaml);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
